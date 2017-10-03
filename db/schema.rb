253fb64c49e8181bc24c1ded88ad211c1f1b9654
Shane McCulley <smcculle@uno.edu>
2017-10-03 18:03:07 -0500

    Changed listings table to reference category by id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928161759) do
+ActiveRecord::Schema.define(version: 20171003223341) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 20170928161759) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "title"
-    t.string   "category"
     t.string   "owner"
     t.string   "images"
     t.string   "tags"
@@ -30,6 +29,8 @@ ActiveRecord::Schema.define(version: 20170928161759) do
     t.float    "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_listings_on_category_id"
   end
 
   create_table "sub_categories", force: :cascade do |t|
