edd907c8ddd0a9254a66afbfba22e2e35c46460a
Shane McCulley <smcculle@uno.edu>
2017-10-19 01:00:19 -0700

    Updated schema to latest migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171003223341) do
+ActiveRecord::Schema.define(version: 20171019020411) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 20171003223341) do
   create_table "listings", force: :cascade do |t|
     t.string   "title"
     t.string   "owner"
-    t.string   "images"
     t.string   "tags"
     t.string   "location"
     t.text     "description"
@@ -31,6 +30,17 @@ ActiveRecord::Schema.define(version: 20171003223341) do
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
     t.index ["category_id"], name: "index_listings_on_category_id"
+  end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+    t.integer  "listing_id"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.index ["listing_id"], name: "index_pictures_on_listing_id"
   end
 
   create_table "sub_categories", force: :cascade do |t|
