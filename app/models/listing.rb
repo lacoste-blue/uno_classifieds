3937788426a67bebb0a00387d401419051bc9406
Shane McCulley <smcculle@uno.edu>
2017-10-18 20:10:38 -0700

    Adds pictures to listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,4 @@
 class Listing < ApplicationRecord
   belongs_to :category
-  has_attached_file :image, styles: {small: "64x64", medium: "100x100", large: "200x200"}
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
-
+  has_many :pictures
 end
