f2a4af0f7e25e1e1e689c3c8523e628950a4e93a
Lance Lacoste <llacoste@iseatz.com>
2017-10-22 11:53:09 -0500

    Restricting to uno.edu domain

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validate :email_domain
+
+  def email_domain
+     domain = email.split("@").last
+     if !email.blank?
+       errors.add(:email, "Invalid Domain") if domain != "uno.edu"
+     end
+  end
 end
