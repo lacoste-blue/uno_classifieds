72031f0bf3fd687b1a2bed078e0e7dee35dd5bba
Shane McCulley <smcculle@uno.edu>
2017-11-01 21:36:39 -0700

    Adds has_pictures scope

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,5 @@ class Listing < ApplicationRecord
   has_many :pictures, :dependent => :destroy
 
   scope :by_category, -> (category_id) { where(:category_id => category_id) }
+  scope :has_picture, -> { includes(:pictures).where.not(pictures: {id: nil}) }
 end
