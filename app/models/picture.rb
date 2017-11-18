8951723022e247bf3e145b631e56d96023c1d017
Shane McCulley <smcculle@uno.edu>
2017-11-17 19:15:36 -0800

    Code quality review

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,14 +1,14 @@
 class Picture < ApplicationRecord
   belongs_to :listing
   has_attached_file :image,
-                    path: ":rails_root/public/images/:id/:filename",
-                    url: "/images/:id/:filename",
+                    path: ':rails_root/public/images/:id/:filename',
+                    url: '/images/:id/:filename',
                     :styles => {
-                        :thumb => "50x50",
-                        :medium => "200x200",
-                        :big => "600x400#",
-                        :large=>"900x350#"
+                        :thumb => '50x50',
+                        :medium => '200x200',
+                        :big => '600x400#',
+                        :large=> '900x350#'
   }
 
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
+  validates_attachment_content_type :image, :content_type => %w(image/jpg image/jpeg image/png)
 end
