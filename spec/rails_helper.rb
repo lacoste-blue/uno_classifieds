f51a4b8fd3e6d2820636e34a1b4c66be93f9be65
Lance Lacoste <llacoste@iseatz.com>
2017-11-19 15:29:24 -0600

    Getting requests working with sign_in

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,7 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'devise'
 require_relative 'support/controller_macros'
+require_relative 'support/request_macros'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -59,4 +60,5 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
   config.include FactoryBot::Syntax::Methods
+  config.include RequestMacros, type: :request
 end
--- spec/requests/pictures_spec.rb
+++ spec/requests/pictures_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Pictures', type: :request do
   describe 'GET /pictures' do
     it 'works! (now write some real specs)' do
+      sign_in FactoryBot.create(:admin)
       get pictures_path
       expect(response).to have_http_status(200)
     end
