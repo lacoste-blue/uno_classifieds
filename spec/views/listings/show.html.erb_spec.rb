bc0931b4017a95f6c81a7dfcb08a17f77a42ac00
Shane McCulley <smcculle@uno.edu>
2017-11-20 15:59:24 -0800

    Fix test error, must have user to create listing

--- spec/views/listings/show.html.erb_spec.rb
+++ spec/views/listings/show.html.erb_spec.rb
@@ -1,20 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe 'listings/show', type: :view do
+RSpec.describe 'listings/show', :type => :view do
   before(:each) do
     FactoryBot.create(:category)
     @listing = assign(:listing, Listing.create!(
-      :title => 'Title Woah!',
-      :category_id => 1,
-      :user_id => 1,
-      :location => 'Location',
-      :description => 'MyText',
-      :price => 2.5
+                                  :title => 'Title Woah!',
+                                  :category_id => 1,
+                                  :user_id => 1,
+                                  :location => 'Location',
+                                  :description => 'MyText',
+                                  :price => 2.5
     ))
   end
 
   it 'renders attributes in <p>' do
+    FactoryBot.create(:user)
     render
     expect(rendered).to match(/Title Woah!/)
   end
 end
+
