4b1d98c6fb1120fdd327a4820b42ae42da7bcb2c
Shane McCulley <smcculle@uno.edu>
2017-11-22 22:41:15 -0800

    Add links to listings description

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -11,17 +11,37 @@ module ListingsHelper
     "Hi,\n\nI am interested in your listing titled #{listing.title} on UNO Classifieds.\n\n"
   end
 
+  def self.category_id(listing, context, param)
+    cat_name = listing.category.name.titleize
+    %(in the #{context.make_link(cat_name, param)} category ).html_safe
+  end
+
+  def self.tag(_, context, param)
+    %(with tags like #{context.make_link(param.values.first, param)} ).html_safe
+  end
+
+  def self.user_id(listing, context, param)
+    name = listing.user.name
+    %(posted by #{context.make_link(name, param)} ).html_safe
+  end
+
+  def self.search(_, _, param)
+    "with description like #{param[:search]} "
+  end
+
+  def make_link(string = nil, param = {})
+    %( #{link_to(string, listings_path(param))} ).html_safe
+  end
+
   def index_welcome(params)
-    welcome = 'Listings %s %s %s'
-    if params[:category_id]
-      format(welcome, 'in the ', Category.find(params[:category_id]).name.titleize, ' category')
-    elsif params[:tag]
-      format(welcome, 'tagged with the ', params[:tag], ' tag')
-    elsif params[:user_id]
-      format(welcome, 'posted by ', User.find(params[:user_id]).name, '')
-    else
-      'All Listings'
+    filter_params = controller.send(:redirect_params, params)
+    return 'No Listings found' if @listings.empty?
+    return make_link('Listings').to_s if filter_params.empty?
+    welcome = %(#{make_link('Listings')} ).html_safe
+    filter_params.each do |key, value|
+      welcome += ListingsHelper.public_send(key, @listings.first, self, key => value) if value.present?
     end
+    welcome
   end
 end
 
